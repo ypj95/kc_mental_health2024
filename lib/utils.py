2c753d47813c32ae0fc88699fdcffaeca6ab2c56
Yves Neyraud <yves.neyraud@outlook.com>
2024-11-06 20:17:01 +0100

    add helper function setup evaluate

--- lib/utils.py
+++ lib/utils.py
@@ -1,2 +1,18 @@
-def test():
-    print("hi")
+def load_processed_datasets(path: Path) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Loads the processed datasets, train and test
+
+    Args:
+        path (Path): _description_
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: _description_
+    """
+    pass
+
+def setup_model(model: str, hyperparameters: dict):
+        if model == "xgboost":
+            return xgb.XGBClassifier(**hyperparameters)
+        elif model == "catboost":
+            return cat.CatBoostClassifier(**hyperparameters)
+        else:
+            raise ValueError(f"Model: {model} not implemented.")
