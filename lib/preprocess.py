613d6a3b44241968023a1d3f855bcd591c4d3789
Yves Neyraud <yves.neyraud@outlook.com>
2024-11-10 13:32:10 +0100

    add sleep duration preprocessing

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = [x.lower().replace(" ", "_") for x in df.columns.to_list()]
@@ -20,3 +21,33 @@ def clean_column_diet_habits(df: pd.DataFrame) -> pd.DataFrame:
         "Less Healthy": "Unhealthy"
     }
     return df.assign(diet=df["diet"].apply(lambda x: diet_habit_mapping.get(x, "nan")))
+
+def calculate_avg_sleep(x):
+    hour = re.search(r'(\d+-?\d*)', x)
+    if hour == None:
+        return float('nan')
+    splits = hour.group(1).split("-")
+    
+    # if real range is given
+    if len(splits) > 1:
+        avg_sleep = int(splits[0]) + int(splits[1])/2
+    else:
+        avg_sleep = int(splits[0])
+    
+    return avg_sleep
+
+
+def clean_sleep_column(df: pd.DataFrame, bin=False) -> pd.DataFrame:
+    """Returns sleep duration as average or category from 0 to 5
+
+    Args:
+        df (pd.DataFrame): _description_
+        bin (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    df["sleep_duration"] = df["sleep_duration"].apply(calculate_avg_sleep).dropna()
+    if bin:
+        df["sleep_duration"] = pd.cut(df["sleep_duration"], 5, labels=range(5))
+    return df
